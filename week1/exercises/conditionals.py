d751a8ebfc3b0c09684df640c1fb8e3e711572c1
ChrisChrisi <chris1cros@gmail.com>
2015-06-06 23:41:13 +0300

    conditionals

---
+++ week1/exercises/conditionals.py
@@ -0,0 +1,24 @@
+def greet(friend, money):
+    if friend and (money > 20):
+        print "Hi!"
+        money -= 20
+    elif friend:
+        print "Hello"
+    else:
+        print "Ha ha"
+        money += 10
+    return money
+
+money = 15
+
+money = greet(True, money)
+print "Money:", money
+print ""
+
+money = greet(False, money)
+print "Money:", money
+print ""
+
+money = greet(True, money)
+print "Money:", money
+print ""
