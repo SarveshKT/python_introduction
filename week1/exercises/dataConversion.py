596386dde183f47515035d9205c70e80ff4bb289
SarveshKT <59925834+SarveshKT@users.noreply.github.com>
2020-09-30 20:33:22 +0530

    Update dataConversion.py

--- week1/exercises/dataConversion.py
+++ week1/exercises/dataConversion.py
@@ -5,6 +5,6 @@
 hour = 3
 ones = hour % 10
 tens = hour // 10
-print tens, ones, ":00"
-print str(tens), str(ones), ":00"
-print str(tens) + str(ones) + ":00"
+print (tens, ones, ":00")
+print (str(tens), str(ones), ":00")
+print (str(tens) + str(ones) + ":00")
